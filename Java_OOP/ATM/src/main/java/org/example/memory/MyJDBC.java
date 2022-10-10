d5b162c6b3da34de317df308bb24e705e32cc082
IrenaShahini <irenashahini.ee@gmail.com>
2022-10-10 17:18:37 +0200

    ATM

--- Java_OOP/ATM/src/main/java/org/example/memory/MyJDBC.java
+++ Java_OOP/ATM/src/main/java/org/example/memory/MyJDBC.java
@@ -4,108 +4,97 @@ import java.sql.*;
 import java.util.Scanner;
 public class MyJDBC {
     public static void main(String args[]) {
-
+        int IBAN, pin, newPin;
         System.out.println("Type your IBAN: ");
         Scanner scanner = new Scanner(System.in);
-        int IBAN;
         IBAN = scanner.nextInt();
+        System.out.println("Type your PIN: ");
+        pin = scanner.nextInt();
+        double newBalance;
         int chosen;
         double amount;
-        do {
-            welcome(IBAN);
-            System.out.println("Please choose one of the following options: ");
-            System.out.println("1. Show your balance. ");
-            System.out.println("2. Withdraw money. ");
-            System.out.println("3. Deposit money. ");
-            System.out.println("4. Change your PIN. ");
-//            System.out.println("5. Transfer money to another person.");
-            System.out.println("0. Exit. ");
-            chosen = scanner.nextInt();
-            switch (chosen) {
-                case 1:
-                    showBalance(IBAN);
-                    break;
-                case 2:
-                    System.out.println("Please enter the amount of money that you want to withdraw: ");
-                    amount = scanner.nextDouble();
-//                    desc = "Withdraw";
-//                    Withdraw w = new Withdraw(amount, "Withdraw", desc);
-//                    bankAccount.withdraw(w.amount);
-                    break;
-                case 3:
-                    System.out.println("Please enter the amount of money that you want to deposit: ");
-                    amount = scanner.nextDouble();
-//                    desc = "Withdraw";
-//                    d = new Deposit(amount, "Deposit", desc);
-//                    bankAccount.deposit(d.amount);
-                    break;
-                case 4:
-//                    bankAccount.ChangePIN();
-                    break;
-            }
-            ;
-        } while (chosen != 0);
-        System.exit(0);
-    }
-//    Person person = new Person(2,"email@gmail.com","Irena","Shahin i");
-//    Card card = new Card(1234,"debit",1234);
-//    BankAccount bank = new BankAccount(85498, person, card,new ArrayList<Operation>(),12569.80);
-
-
-//        Card card = new Card(123455, "debit", 2567);
-//        BankAccount bank = new BankAccount(2569745, card, new Person(3264, "email@gmail.com", "Irena", "Shahin i"), new ArrayList<Operation>(), 12569.80);
-
-//        try {
-//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "MySQLPass");
-//            Statement statement = connection.createStatement();
-//            ResultSet resultSet = statement.executeQuery("select * from bank_account");
-//            while (resultSet.next()) {
-//                System.out.println(resultSet.getString("IBAN"));
-//
-//
-////            DELETE FROM table_name WHERE condition;
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-//        welcome(2569745);
-
-
-
-//    methods
-
-    public static void welcome (int IBAN){
-            try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "MySQLPass");
-
-                String query ="SELECT firstname,lastname from bank_account where IBAN =?";
-
-                PreparedStatement statement = connection.prepareStatement("SELECT firstname,lastname from bank_account where IBAN =?");
-                statement.setInt(1, IBAN);
-                ResultSet resultSet = statement.executeQuery();
-                if(resultSet.next()) {
-                    System.out.println("Hello " + resultSet.getString("firstname") + " " + resultSet.getString("lastname") + " welcome to your Bank Account.");
-                }
-                } catch (Exception e) {
-                System.out.println(e);
-            }
-//        menu();
-        }
-    private static void showBalance(int IBAN) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "MySQLPass");
-            String query = "SELECT firstname,lastname from bank_account where IBAN =?";
-
-            PreparedStatement statement = connection.prepareStatement("SELECT * from bank_account where IBAN =?");
+            Connection connection;
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "MySQLPass");
+//            String query = "SELECT firstname,lastname from bank_account where IBAN =?";
+            PreparedStatement statement;
+            statement = connection.prepareStatement("SELECT * from bank_account where IBAN =? AND pin=?");
             statement.setInt(1, IBAN);
-            ResultSet resultSet = statement.executeQuery();
+            statement.setInt(2, pin);
+            ResultSet resultSet;
+            resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                System.out.println(resultSet.getString("firstname") + " " + resultSet.getString("lastname") + " your Balance is: " + resultSet.getDouble(9));
+                System.out.println("Hello " + resultSet.getString("firstname") + " " + resultSet.getString("lastname") + " welcome to your Bank Account.");
+                double balance = resultSet.getDouble(9);
+                do {
+                    System.out.println("Please choose one of the following options: ");
+                    System.out.println("1. Show your balance. ");
+                    System.out.println("2. Withdraw money. ");
+                    System.out.println("3. Deposit money. ");
+                    System.out.println("4. Change your PIN. ");
+//            System.out.println("5. Transfer money to another person.");
+                    System.out.println("0. Exit. ");
+                    chosen = scanner.nextInt();
+                    switch (chosen) {
+                        case 1:
+                            statement = connection.prepareStatement("SELECT * from bank_account where IBAN =?");
+                            statement.setInt(1, IBAN);
+                            resultSet = statement.executeQuery();
+                            if(resultSet.next()) {
+                                System.out.println(resultSet.getString("firstname") + " " + resultSet.getString("lastname") + " your Balance is: " + resultSet.getDouble(9));
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Please enter the amount of money that you want to withdraw: ");
+                            amount = scanner.nextDouble();
+                            if(amount <= balance) {
+                                statement = connection.prepareStatement("SELECT * from bank_account where IBAN =?");
+                                statement.setInt(1, IBAN);
+                                resultSet = statement.executeQuery();
+                                if(resultSet.next()) {
+                                  newBalance = resultSet.getDouble(9) - amount;
+                                } else newBalance = resultSet.getDouble(9);
+                                statement = connection.prepareStatement("UPDATE bank_account SET balance = ? WHERE IBAN = ?");
+                                statement.setDouble(1, resultSet.getDouble(9) - amount);
+                                statement.setInt(2, IBAN);
+                                statement.execute();
+                                System.out.println("Your new balance is: " + newBalance);
+                            }else System.out.println("Your balance is lower");
+                            break;
+                        case 3:
+                            System.out.println("Please enter the amount of money that you want to deposit: ");
+                            amount = scanner.nextDouble();
+                            statement = connection.prepareStatement("SELECT * from bank_account where IBAN =?");
+                            statement.setInt(1, IBAN);
+                            resultSet = statement.executeQuery();
+                            if(resultSet.next()) {
+                                newBalance = resultSet.getDouble(9) + amount;
+                            } else newBalance = resultSet.getDouble(9);
+                            statement = connection.prepareStatement("UPDATE bank_account SET balance = ? WHERE IBAN = ?");
+                            statement.setDouble(1, resultSet.getDouble(9) + amount);
+                            statement.setInt(2, IBAN);
+                            statement.executeUpdate();
+                            System.out.println("Your new balance is: " + newBalance);
+                            break;
+                        case 4:
+//                    bankAccount.ChangePIN();
+                            break;
+                    }
+                    ;
+                } while (chosen != 0);
+                System.exit(0);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 }
+//    Person person = new Person(2,"email@gmail.com","Irena","Shahin i");
+//    Card card = new Card(1234,"debit",1234);
+//    BankAccount bank = new BankAccount(85498, person, card,new ArrayList<Operation>(),12569.80);
+
+//        Card card = new Card(123455, "debit", 2567);
+//        BankAccount bank = new BankAccount(2569745, card, new Person(3264, "email@gmail.com", "Irena", "Shahin i"), new ArrayList<Operation>(), 12569.80);
+
 
 
--- Java_OOP/ATM/src/main/java/org/example/services/BankAccount.java
+++ Java_OOP/ATM/src/main/java/org/example/services/BankAccount.java
@@ -6,6 +6,7 @@ import org.example.model.Person;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BankAccount {
     private int IBAN;
@@ -42,48 +43,36 @@ public class BankAccount {
     }
 
         public void showBalance () {
-            try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/atm", "root", "MySQLPass");
-                String query = "SELECT firstname,lastname from bank_account where IBAN =?";
-
-                PreparedStatement statement = connection.prepareStatement("SELECT firstname,lastname from bank_account where IBAN =?");
-                statement.setInt(1, this.IBAN);
-                ResultSet resultSet = statement.executeQuery();
-                if (resultSet.next()) {
-                    System.out.println(resultSet.getString("firstname") + " " + resultSet.getString("lastname") + " your Balance is: " + resultSet.getDouble("balance"));
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            }
+            System.out.println("Your balance is: " + balance);
         }
 
-//        public void withdraw ( double amount){
-//            if (amount <= balance) {
-//                balance = balance - amount;
-//                System.out.println("New Balance is: " + balance);
-//            } else {
-//                System.out.println("Something wrong!");
-//            }
-//        }
-//        public void deposit ( double amount){
-//            balance = balance + amount;
-//            System.out.println("New Balance is: " + balance);
-//        }
-//    public void ChangePIN(){
-//        int newPIN, oldPIN;
-//        System.out.println("Please enter your account's Pin: ");
-//        Scanner s1 = new Scanner(System.in);
-//        oldPIN = s1.nextInt();
-//        if (oldPIN == card.PIN){
-//            System.out.println("Enter new PIN: ");
-//            newPIN = s1.nextInt();
-//            if(newPIN >= 1000 && newPIN <=9999){
-//                card.PIN = newPIN;
-//                System.out.println("New PIN is " + newPIN);
-//            }
-//            System.out.println("PIN must have 4 numbers, min 1000!");
-//        }else
-//            System.out.println("Wrong PIN");
-//    }
+        public void withdraw ( double amount){
+            if (amount <= balance) {
+                balance = balance - amount;
+                System.out.println("New Balance is: " + balance);
+            } else {
+                System.out.println("Something wrong!");
+            }
+        }
+        public void deposit ( double amount){
+            balance = balance + amount;
+            System.out.println("New Balance is: " + balance);
+        }
+    public void ChangePIN(){
+        int newPIN, oldPIN;
+        System.out.println("Please enter your account's Pin: ");
+        Scanner s1 = new Scanner(System.in);
+        oldPIN = s1.nextInt();
+        if (oldPIN == card.getPin()){
+            System.out.println("Enter new PIN: ");
+            newPIN = s1.nextInt();
+            if(newPIN >= 1000 && newPIN <=9999){
+                card.setPin(newPIN);
+                System.out.println("New PIN is " + newPIN);
+            }
+            System.out.println("PIN must have 4 numbers, min 1000!");
+        }else
+            System.out.println("Wrong PIN");
+    }
 
 }
